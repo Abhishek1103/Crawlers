e4c6bdfba55ac60c20f698ed203b406b09be895a
Abhishek Kumar Singh <kumar.abh494@gmail.com>
2018-02-09 02:09:47 +0530

    added serialCrawler2.py (only saves distinct links)

---
+++ BasicCrawlers/serialCrawler2.py
@@ -0,0 +1,56 @@
+import requests as req
+from bs4 import BeautifulSoup
+from queue import *
+
+proxy = {"http": "http://edcguest:edcguest@172.31.52.54:3128/",
+                "https": "http://edcguest:edcguest@172.31.52.54:3128/"}
+
+#q = Queue()
+st = set()
+st2 = set()
+print("Enter initial URL: ", end="")
+
+linksFile = open("DistinctLinks","w")
+initialUrl = "https://timesofindia.indiatimes.com"
+#initialUrl = "https://www.youtube.com/"
+initialUrl = input()
+source = req.get(initialUrl, proxies=proxy)
+data = source.text
+soup = BeautifulSoup(data, "html.parser")
+
+for link in soup.find_all('a'):
+    s = str(link.get('href'))
+    if(s[0] == 'h'):
+        print(str(s))
+        st.add(s)
+        #st2.add(s)
+        #linksFile.write(s+"\n")
+i=0
+try:
+    while i < 30000:
+        url = st.pop()
+        linksFile.write(str(url)+"\n")
+        source = req.get(url, proxies=proxy)
+        data = source.text
+        soup = BeautifulSoup(data, "html.parser")
+
+        for link in soup.find_all('a'):
+            s = str(link.get('href'))
+            try:
+                if(s[0] == 'h'):
+                    #print(str(s))
+                    st.add(s)
+                    #st2.add(s)
+                    i += 1
+                    if i%1000 == 0 :
+                        print(i)
+            except Exception as ex:
+                print(str(ex)+": Invalid URL")
+    for lnk in st:
+        linksFile.write(str(lnk)+"\n")
+except Exception as ex:
+    linksFile.close()
+    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+    message = template.format(type(ex).__name__, ex.args)
+    print(message)
+    print("EXITING..!!")
